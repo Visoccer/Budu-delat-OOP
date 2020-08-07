1eff66cea56a871555936176669c62913ad13958
Visoccer <sokarev.ivan@mail.ru>
2020-08-07 18:08:22 +0300

    update main.cpp

--- phonebook/main.cpp
+++ phonebook/main.cpp
@@ -1,153 +1,187 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+
+//using namespace std;
+
+class Number {
+public:
+    Number(std::string data) : data_(data) {}
+    Number() {}
+    
+    std::string GetData() {
+        return data_;
+    }
+private:
+    std::string data_;
+};
+ 
+class Name {
+public:
+    Name(std::vector<std::string> data) : data_(data) {}
+    Name() {}
+    
+    std::vector<std::string> GetData() {
+        return data_;
+    }
+private:
+    std::vector<std::string> data_;
+};
+ 
+class Adress {
+public:
+    Adress(std::string data) : data_(data) {}
+    Adress() {}
+
+    std::string GetData() {
+        return data_;
+    }
+private:
+    std::string data_;
+};
+
+std::vector<std::string> ConvertToVector(const std::string words) {
+    std::vector<std::string> convertedVector;
+    std::string currentWord;
+    for (int i = 0; i < words.size(); i++) {
+        char symbol = words[i];
+        if (symbol >= 'A' && symbol <= 'Z') {
+            symbol = char('a' + int(symbol - 'A'));
+        }
+        if (symbol != ' ') {
+            convertedVector.push_back(currentWord);
+            currentWord.clear();
+        }
+        else {
+            currentWord.push_back(symbol);
+        }
+    }
+    if (!currentWord.empty()) {
+        convertedVector.push_back(currentWord);
+    }
+    sort(convertedVector.begin(), convertedVector.end());
+    return convertedVector;
+}
+
+class Contact {
+public:	
+    Contact(Number number, Name name, Adress adress) : 
+        number_(number), name_(name), adress_(adress) {}
+    Contact() {}
+    
+    //getters
+    std::string GetNumber() {
+        return number_.GetData();
+    }
+    std::vector<std::string> GetName() {
+        return name_.GetData();
+    }
+    std::string GetAdress() {
+        return adress_.GetData();
+    }
+    
+    //setters
+    void SetNumber(const std::string number) {
+        number_ = number;
+    }
+    void SetName(const std::string name) {
+        std::vector<std::string> convertedName = ConvertToVector(name);
+        name_ = convertedName;
+    }
+    void SetAdress(const std::string adress) {
+        adress_ = adress;
+    }
+
+private:
+    Number number_;
+    Name name_;
+    Adress adress_;
+};
  
 class PhoneBook {
 public:
-	struct Contact {
-		std::string phoneNumber_, name_, adress_;
-		int id_;
-		Contact(std::string phoneNumber, std::string name, std::string adress, int id) : phoneNumber_(phoneNumber), name_(name), adress_(adress), id_(id) {} 
-	};
-	void PrintContact(const Contact& currentContact) const {
-		std::cout << currentContact.phoneNumber_ << ' ' << currentContact.name_ << ' ' << currentContact.adress_ << ' ' << currentContact.id_ << "\n";
-	}
-	bool Check(const std::string number, const std::string sub_number) {
-		return (sub_number.length() <= number.length() && number.substr((int)sub_number.length()) == sub_number);
-	}
-	void AddNew() {
-		std::string newNumber, newName, newAdress;
-		std::cout << "Enter his phone number:\n";
-		std::cin >> newNumber;
-		std::cout << "Enter his name:\n";
-		std::cin >> newName;
-		std::cout << "Enter his adress:\n";
-		std::cin >> newAdress;
-		int wasId = 0;
-		for (auto& currentContact : contacts_) {
-			if (currentContact.name_ == newName || currentContact.phoneNumber_ == newNumber) {
-				wasId = currentContact.id_;
-				break;
-			}
-		}
-		if (wasId != 0) {
-			std::cout << "There is such contact with ID " << wasId << "\n"; 
-		}
-		else {
-			contacts_.push_back({newNumber, newName, newAdress, ++count_});
-		}
-	}
-	void FindByName() {
-		
-	}
-	void FindByPhoneNumber() {
-		std::cout << "Enter what phone number to find:\n";
-		std::string numberForFind;
-		std::cin >> numberForFind;
-		std::vector<Contact> suitable;
-		for (auto& currentContact : contacts_) {
-			if (Check(currentContact.phoneNumber_, numberForFind)) {
-				suitable.push_back(currentContact);
-			}
-		}
-		if (suitable.empty()) {
-			std::cout << "There are no contacts with such phone number\n";
-		}
-		else {
-			for (auto& current_contact : suitable) {
-				PrintContact(current_contact);
-			}
-		}
-	}
-	void FindById() {
-		std::cout << "What ID do you want to find?\n";
-		int findID;
-		bool was = false;
-		std::cin >> findID;
-		for (auto& currentContact : contacts_) {
-			if (currentContact.id_ == findID) {
-				PrintContact(currentContact);
-				was = true;
-				break;
-			}
-		}
-		if (!was) {
-			std::cout << "There is no such ID\n";
-		}
-	}
-	void DeleteById() {
-		std::cout << "What ID do you want to delete?\n";
-		int delID, need = -1;
-		std::cin >> delID;
-		for (int number = 0; number < contacts_.size(); number++) {
-			if (contacts_[number].id_ == delID) {
-				need = number;
-				break;
-			}
-		}
-		if (need == -1) {
-			std::cout << "There is no such ID\n";
-		}
-		else {
-			contacts_.erase(contacts_.begin() + need);
-		}
-	}
-	void ShowContacts() {
-		for (auto& currentContact : contacts_) {
-			PrintContact(currentContact);
-		}
-	}
+    int Check(const std::string number, const std::vector<std::string> name) {
+        for (auto& iteration : contacts_) {
+            auto& contact = iteration.second;
+            if (contact.GetNumber() == number || contact.GetName() == name) {
+                return iteration.first;
+            }
+        }
+        return -1;
+    }
+    
+    bool CheckAdress(const std::string subAdress, const std::string adress) {
+        int len = subAdress.length();
+        return (len <= adress.length() && subAdress == adress.substr(0, len));
+    }
+    
+    int AddContact(const std::string number, const std::string name, const std::string adress) {
+        std::vector<std::string> convertedName = ConvertToVector(name);
+        int check = Check(number, convertedName);
+        if (check != -1) {
+            return check;
+        }
+        else {
+            Contact newContact = {number, convertedName, adress};
+            contacts_[++count_] = {number, convertedName, adress};
+            return 0;
+        }
+    }
+    
+    int FindName(const std::string name) {
+        std::vector<std::string> convertedName = ConvertToVector(name);
+        for (auto& iteration : contacts_) {
+            auto& contact = iteration.second;
+            if (contact.GetName() == convertedName) {
+                return iteration.first;
+            }
+        }
+        return -1;
+    }
+    
+    int FindNumber(const std::string number) {
+        for (auto& iteration : contacts_) {
+            auto& contact = iteration.second;
+            if (contact.GetNumber() == number) {
+                return iteration.first;
+            }
+        }
+        return -1;
+    }
+    
+    Contact FindID(const int id) {
+        return contacts_[id];
+    }
+    
+    void DeleteID(const int id) {
+        contacts_.erase(contacts_.find(id));
+    }
+    
+    std::vector<Contact> FindAdress(const std::string adress) {
+        std::vector<Contact> finded;
+        for (auto& iteration : contacts_) {
+            auto& contact = iteration.second;
+            std::string currentAdress = contact.GetAdress();
+            if (CheckAdress(adress, currentAdress)) {
+                finded.push_back(contact);
+            }
+        }
+        return finded;
+    }
+    
 private:
-    std::vector<Contact> contacts_;
+    std::unordered_map<int, Contact> contacts_;
     int count_ = 0;
 };
  
-int ChooseAction() {
-	std::cout << "Choose action:\n";
-	std::cout << "1. Add contact\n";
-	std::cout << "2. Find contact by name\n";
-	std::cout << "3. Find contact by phone number\n";
-	std::cout << "4. Find contact by id\n";
-	std::cout << "5. Delete contact by id\n";
-	std::cout << "6. Show contacts with the same adress\n";
-	int action;
-	std::cin >> action;
-	return action;
-}
- 
 void Solve() {
     PhoneBook phonebook;
-	int action = ChooseAction();
-//	std::cout << "DEBUG : " << action << std::endl;
-	switch (action) {
-		case 1: {
-			phonebook.AddNew();
-			break;
-		}
-		case 2: {
-			phonebook.FindByName();
-			break;
-		}
-		case 3: {
-			phonebook.FindByPhoneNumber(); 
-			break;
-		}
-		case 4: {
-			phonebook.FindById();
-			break;
-		}
-		case 5: {
-			phonebook.DeleteById();
-			break;
-		}
-		case 6: {
-			phonebook.ShowContacts();
-			break;
-		}
-	}
 }
  
 int main() {
-	Solve();
-	return 0;
+    Solve();
+    return 0;
 }
